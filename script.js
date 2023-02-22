9f9970a85ca0f12da97f36c14022a00c405b0037
R3wi <remi@hub01.org>
2023-02-22 13:07:21 -0500

    ajutement

--- script.js
+++ script.js
@@ -56,12 +56,11 @@ function init() {
                         //console.log('element 2:' + ele);
                         if(ind == 2){
                             nomArtistes.push(rowData.c[ind].v);
-
                             
                             const artiste1 = document.getElementById("lien_artiste_1");
                             const artiste1_a = artiste1.getElementsByTagName("a");
                             const artiste1_a_ele = artiste1_a[0];
-                            artiste1_a_ele.textContent = nomArtistes[0];
+                            artiste1_a_ele.textContent = rowData.c[ind].v;
                         }
                     }
                     
@@ -73,34 +72,7 @@ function init() {
             processRows(data);
         })
 
-        console.log('début');
-
-        const artiste1 = document.getElementById("lien_artiste_1");
-        const artiste1_a = artiste1.getElementsByTagName("a");
-        const artiste1_a_ele = artiste1_a[0];
-        artiste1_a_ele.textContent = nomArtistes[0];
-        const artiste2 = document.getElementById("lien_artiste_2");
-        const artiste2_a = artiste2.getElementsByTagName("a");
-        const artiste2_a_ele = artiste2_a[0];
-        artiste2_a_ele.textContent = nomArtistes[1];
-        const artiste3 = document.getElementById("lien_artiste_3");
-        const artiste3_a = artiste3.getElementsByTagName("a");
-        const artiste3_a_ele = artiste3_a[0];
-        artiste3_a_ele.textContent = nomArtistes[2];
-        const artiste4 = document.getElementById("lien_artiste_4");
-        const artiste4_a = artiste4.getElementsByTagName("a");
-        const artiste4_a_ele = artiste4_a[0];
-        artiste4_a_ele.textContent = nomArtistes[3];
-        const artiste5 = document.getElementById("lien_artiste_5");
-        const artiste5_a = artiste5.getElementsByTagName("a");
-        const artiste5_a_ele = artiste5_a[0];
-        artiste5_a_ele.textContent = nomArtistes[4];
-
-        console.log('fin');
-
-        $('#lien_artiste_1 a').html('coucou');
-        console.log(nomArtistes[0]);
-        $('#lien_artiste_1 a').html(nomArtistes[0]);
+        
 }
 
 function processRows(json) {
@@ -119,29 +91,5 @@ function processRows(json) {
 
 $( document ).ready(function() {
     console.log('jquery document ready');
-    const artiste1 = document.getElementById("lien_artiste_1");
-        const artiste1_a = artiste1.getElementsByTagName("a");
-        const artiste1_a_ele = artiste1_a[0];
-        artiste1_a_ele.textContent = nomArtistes[0];
-        const artiste2 = document.getElementById("lien_artiste_2");
-        const artiste2_a = artiste2.getElementsByTagName("a");
-        const artiste2_a_ele = artiste2_a[0];
-        artiste2_a_ele.textContent = nomArtistes[1];
-        const artiste3 = document.getElementById("lien_artiste_3");
-        const artiste3_a = artiste3.getElementsByTagName("a");
-        const artiste3_a_ele = artiste3_a[0];
-        artiste3_a_ele.textContent = nomArtistes[2];
-        const artiste4 = document.getElementById("lien_artiste_4");
-        const artiste4_a = artiste4.getElementsByTagName("a");
-        const artiste4_a_ele = artiste4_a[0];
-        artiste4_a_ele.textContent = nomArtistes[3];
-        const artiste5 = document.getElementById("lien_artiste_5");
-        const artiste5_a = artiste5.getElementsByTagName("a");
-        const artiste5_a_ele = artiste5_a[0];
-        artiste5_a_ele.textContent = nomArtistes[4];
-        console.log('jquery fin');
-
-        $('#lien_artiste_1 a').html('coucou');
-        console.log(nomArtistes[0]);
-        $('#lien_artiste_1 a').html(nomArtistes[0]);
+    
 });
